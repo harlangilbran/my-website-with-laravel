d802924c385ef2f5c44737924e07014a01ed57c8
Harlan Gilbran <harlangilbran12@gmail.com>
2025-02-12 05:04:51 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -7,13 +8,52 @@ Route::get('/', function () {
 });
 
 Route::get('/about', function () {
-    return view('about', ['name' => 'Harlan Gilbran', 'title' => 'Contact Page']);
+    return view('about', ['name' => 'Harlan Gilbran', 'title' => 'About']);
 });
 
-Route::get('/blog', function () {
-    return view('blog' , ['title' => 'Blog Page']);
+Route::get('/posts', function () {
+    return view('posts' , ['title' => 'Blog', 'posts' => [
+        [
+            'id' => '1',
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul artikel 1',
+            'author' => 'Harlan Gilbran',
+            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae ut possimus incidunt itaque porro maiores commodi deserunt sapiente adipisci, nisi dolore molestias facere laudantium ad quos! Deserunt, libero neque? Veritatis!'
+        ],
+         [
+            'id' => '2',
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul artikel 2',
+            'author' => 'Harlan Gilbran',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste earum ipsam reprehenderit asperiores, a aliquid itaque alias officiis inventore accusamus minus beatae enim molestiae qui odio ullam provident rerum magni.'
+        ]
+    ]]);
 });
+
+Route::get('/posts/{slug}', function ($slug) {
+    $posts = [
+        [
+            'id' => '1',
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul artikel 1',
+            'author' => 'Harlan Gilbran',
+            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae ut possimus incidunt itaque porro maiores commodi deserunt sapiente adipisci, nisi dolore molestias facere laudantium ad quos! Deserunt, libero neque? Veritatis!'
+        ],
+         [
+            'id' => '2',
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul artikel 2',
+            'author' => 'Harlan Gilbran',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste earum ipsam reprehenderit asperiores, a aliquid itaque alias officiis inventore accusamus minus beatae enim molestiae qui odio ullam provident rerum magni.'
+        ]
+         ];
+
+         $post = Arr::first($posts, function($post) use ($slug) {
+            return $post['slug'] == $slug;
+         });
+        return view('post', ['title' => 'single post', 'post' => $post]);
+}); 
 
 Route::get('/contact', function () {
-    return view('contact', ['title' => 'Contact Page']);
+    return view('contact', ['title' => 'Contact']);
 });
