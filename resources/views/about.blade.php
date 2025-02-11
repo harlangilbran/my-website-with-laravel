b6d04bbcc834d9daab9ac278b8e677388f97f172
Harlan Gilbran <harlangilbran12@gmail.com>
2025-02-12 05:22:46 +0700

    update commit

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,5 +1,6 @@
 <x-layout>
   <x-slot:title>{{ $title }}</x-slot:title>
-  <h3 class="text-xl">Ini adalah Halaman  About Page</h3>
+  <h3 class="text-xl">Ini adalah Halaman  About page</h3>
+  {{-- test --}}
   <p>Nama: {{ $name }}</p>
 </x-layout>
