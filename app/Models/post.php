f9d852a67347455b11ff9a83ab3562652e4c7275
Harlan Gilbran <harlangilbran12@gmail.com>
2025-02-13 02:38:52 +0700

    update

--- app/Models/post.php
+++ app/Models/post.php
@@ -26,11 +26,16 @@ class Post
          ];
     }
 
-    public static function find($slug)
+    public static function find($slug): array
     {
         // return Arr::first(static::all(), function($post) use ($slug) {
         //     return $post['slug'] == $slug;
         //  });
-        return Arr::first(static::all(), fn ($post) => $post['slug'] == $slug);
+        $post = Arr::first(static::all(), fn ($post) => $post['slug'] == $slug);
+
+        if (! $post) {
+            abort(404);
+        }
+        return $post;
     }
 }
