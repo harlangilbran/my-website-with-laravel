8dfc904f81fa196cfd75ae036c6b45efc03f27ee
Harlan Gilbran <harlangilbran12@gmail.com>
2025-02-13 02:06:29 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -3,35 +3,11 @@
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home' , ['title' => 'Home Page']);
-});
-
-Route::get('/about', function () {
-    return view('about', ['name' => 'Harlan Gilbran', 'title' => 'About']);
-});
-
-Route::get('/posts', function () {
-    return view('posts' , ['title' => 'Blog', 'posts' => [
-        [
-            'id' => '1',
-            'slug' => 'judul-artikel-1',
-            'title' => 'Judul artikel 1',
-            'author' => 'Harlan Gilbran',
-            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae ut possimus incidunt itaque porro maiores commodi deserunt sapiente adipisci, nisi dolore molestias facere laudantium ad quos! Deserunt, libero neque? Veritatis!'
-        ],
-         [
-            'id' => '2',
-            'slug' => 'judul-artikel-2',
-            'title' => 'Judul artikel 2',
-            'author' => 'Harlan Gilbran',
-            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste earum ipsam reprehenderit asperiores, a aliquid itaque alias officiis inventore accusamus minus beatae enim molestiae qui odio ullam provident rerum magni.'
-        ]
-    ]]);
-});
-
-Route::get('/posts/{slug}', function ($slug) {
-    $posts = [
+class Post 
+{
+    public static function all()
+    {
+        return [
         [
             'id' => '1',
             'slug' => 'judul-artikel-1',
@@ -47,8 +23,24 @@ Route::get('/posts/{slug}', function ($slug) {
             'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste earum ipsam reprehenderit asperiores, a aliquid itaque alias officiis inventore accusamus minus beatae enim molestiae qui odio ullam provident rerum magni.'
         ]
          ];
+    }
+}
 
-         $post = Arr::first($posts, function($post) use ($slug) {
+Route::get('/', function () {
+    return view('home' , ['title' => 'Home Page']);
+});
+
+Route::get('/about', function () {
+    return view('about', ['name' => 'Harlan Gilbran', 'title' => 'About']);
+});
+
+Route::get('/posts', function () {
+    return view('posts' , ['title' => 'Blog', 'posts' => Post::all()]);
+});
+
+Route::get('/posts/{slug}', function ($slug) {
+   
+         $post = Arr::first(Post::all(), function($post) use ($slug) {
             return $post['slug'] == $slug;
          });
         return view('post', ['title' => 'single post', 'post' => $post]);
