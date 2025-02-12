2bc6348ef141946d18fd3e60e4bb1e0305b37190
Harlan Gilbran <harlangilbran12@gmail.com>
2025-02-13 02:12:57 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -3,28 +3,7 @@
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
-class Post 
-{
-    public static function all()
-    {
-        return [
-        [
-            'id' => '1',
-            'slug' => 'judul-artikel-1',
-            'title' => 'Judul artikel 1',
-            'author' => 'Harlan Gilbran',
-            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae ut possimus incidunt itaque porro maiores commodi deserunt sapiente adipisci, nisi dolore molestias facere laudantium ad quos! Deserunt, libero neque? Veritatis!'
-        ],
-         [
-            'id' => '2',
-            'slug' => 'judul-artikel-2',
-            'title' => 'Judul artikel 2',
-            'author' => 'Harlan Gilbran',
-            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste earum ipsam reprehenderit asperiores, a aliquid itaque alias officiis inventore accusamus minus beatae enim molestiae qui odio ullam provident rerum magni.'
-        ]
-         ];
-    }
-}
+
 
 Route::get('/', function () {
     return view('home' , ['title' => 'Home Page']);
