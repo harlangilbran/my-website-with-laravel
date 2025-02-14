2d33a41b9cf25c5b3679635f3e9bb7d6de6efcf6
Harlan Gilbran <harlangilbran12@gmail.com>
2025-02-15 03:37:30 +0700

    update files

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
@@ -17,8 +18,12 @@ Route::get('/posts', function () {
     return view('posts' , ['title' => 'Blog', 'posts' => Post::all()]);
 });
 
-Route::get('/posts/{post:slug}', function (post $post) {   
-        return view('post', ['title' => 'single post', 'post' => $post]);
+Route::get('/posts/{post:slug}', function (Post $post) {   
+        return view('post', ['title' => 'single Post', 'post' => $post]);
+}); 
+
+Route::get('/authors/{user}', function (User $user) {   
+        return view('posts', ['title' => 'Articles by ' . $user->name, 'posts' => $user->posts]);
 }); 
 
 Route::get('/contact', function () {
