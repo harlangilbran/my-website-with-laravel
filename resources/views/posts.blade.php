180d318229be014a9210bdad8cbd71a10d23c788
Harlan Gilbran <harlangilbran12@gmail.com>
2025-02-15 04:16:08 +0700

    update

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,6 +6,7 @@
     <a href="/posts/{{ $post['slug'] }}" class="hover:underline">
     <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2></a>
     <div class="text-base text-gray-500">
+      by
       <a href="/authors/{{ $post->author->id }}" class="hover:underline">{{ $post->author->name }}</a> | {{ $post->created_at->diffForHumans() }}
     </div>
     <p class="my-4 font-light">{{ Str::limit($post['body'], 150) }}</p>
