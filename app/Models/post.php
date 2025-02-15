774b4714c2a15fea5c5f2e6b4073aa5264b10df6
Harlan Gilbran <harlangilbran12@gmail.com>
2025-02-15 20:04:45 +0700

    update file

--- app/Models/post.php
+++ app/Models/post.php
@@ -14,4 +14,8 @@ class Post extends Model
     {
         return $this->belongsTo(User::class);
     }
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
