539e1745627f3b54bcbc16e848c68ae1ccb18876
Harlan Gilbran <167880900+harlangilbran@users.noreply.github.com>
2025-02-19 03:31:17 +0700

    Update posts.blade.php

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,7 +3,6 @@
   
   <div class="py-4 px-4 mx-auto max-w-screen-xl lg:py-8 lg:px-0">
       <div class="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
-
   @foreach ($posts as $post)
           <article class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
               <div class="flex justify-between items-center mb-5 text-gray-500">
@@ -31,8 +30,7 @@
                       <svg class="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                   </a>
               </div>
-          </article>             
-      
+          </article>           
       @endforeach
     </div>  
   </div>
